261eaaacb02c05a9d39a109fb291bf026ef1676d
Maciej Zelek <maciejz4@hotmail.com>
2019-03-07 22:32:54 +0000

    Add gradual acceleration functionality

--- src/main.cpp
+++ src/main.cpp
@@ -53,7 +53,7 @@ int main() {
   
   int lane = 1;
   
-  double ref_vel = 49.5;
+  double ref_vel = 0.0;
 
   h.onMessage([&ref_vel,&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
                &map_waypoints_dx,&map_waypoints_dy,&lane]
@@ -94,6 +94,33 @@ int main() {
           vector<vector<double>> sensor_fusion = j[1]["sensor_fusion"];
           
           int prev_size = previous_path_x.size();
+          
+          if (prev_size > 0) {
+              car_s = end_path_s;
+          }
+          
+          bool too_close = false;
+          
+          for (int i=0; i < sensor_fusion.size(); i++) {
+              float d = sensor_fusion[i][6];
+              if (d < (2+4*lane+2) && d > (2+4*lane-2)) {
+                  double vx = sensor_fusion[i][3];
+                  double vy = sensor_fusion[i][4];
+                  double check_speed = sqrt(vx*vx+vy*vy);
+                  double check_car_s = sensor_fusion[i][5];
+                
+                  check_car_s+=((double)prev_size*.02*check_speed);
+                  if((check_car_s > car_s) && ((check_car_s - car_s) < 30)) {
+                      too_close = true;
+                  }
+              }
+          }
+          
+          if (too_close) {
+              ref_vel -= .224;
+          } else if (ref_vel < 49.5) {
+              ref_vel += .224;
+          }
           
           vector<double> ptsx;
           vector<double> ptsy;
